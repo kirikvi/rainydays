3521060abd9784f9fd9f477baf45e975ce93636a
Kiri M. Kvistnes <71313020+kirikvi@users.noreply.github.com>
2021-03-19 17:32:37 +0100

    Updated contact and payment forms

--- js/contact.js
+++ js/contact.js
@@ -1,33 +1,24 @@
 const form = document.querySelector(".message-form");
-const aName = document.querySelector("#name");
-const nameError = document.querySelector("#name-error");
 const email = document.querySelector("#e-mail");
 const emailError = document.querySelector("#email-error");
 const message = document.querySelector("#message");
 const messageError = document.querySelector("#message-error");
-//const confirmation = document.querySelector(".confirmation");
-const charCounter = document.querySelector(".char-counter");
+const confirmation = document.querySelector(".confirmation");
 const button = document.querySelector("button");
 
 function validateForm(){
     event.preventDefault();
 
-    if(validateLen(aName.value, 0)){
-        nameError.style.display = "none";
-    } else {
-        nameError.style.display = "block";
-    }
-
     if(validateEmail(email.value, 0)){
         emailError.style.display = "none";
     } else {
-        emailError.style.display = "block";
+        emailError.style.display = "inline";
     }
 
     if(validateLen(message.value, 0)){
         messageError.style.display = "none";
     } else {
-        messageError.style.display = "block";
+        messageError.style.display = "inline";
     }
 }
 form.addEventListener("submit", validateForm);
@@ -46,10 +37,14 @@ function validateEmail(email) {
     return matching;
 }
 
-button.onclick = function(){
-    if(validateLen(aName.value, 0) && validateLen(message.value, 500) && validateEmail(email.value, 0)){
-        confirmation.style.display = "block";
+function validationSuccess(){
+    console.log("test");
+    if(validateLen(message.value, 500) && validateEmail(email.value, 0)){
+        confirmation.style.display = "inline";
+        console.log("success");
     } else {
         confirmation.style.display = "none";
+        console.log("no success");
     }
 }
+button.onclick = validationSuccess();
